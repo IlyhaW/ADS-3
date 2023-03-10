1a2765664fbce5120bdecdd0e19b571ef16bd925
IlyhaW <125214907+IlyhaW@users.noreply.github.com>
2023-03-10 16:08:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
-// Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int lBorder = 0;
+    int rBorder = size;
+    int median;
+    int count = 1;
+    while (lBorder < rBorder) {
+        median = (lBorder + rBorder) / 2;
+        if (arr[median] < value)
+            lBorder = median;
+        if (arr[median] > value)
+            rBorder = median + 1;
+        if (arr[median] == value) {
+            int lShift = median - 1;
+            while (arr[lShift] == value && lShift >= 0) {
+                count += 1;
+                lShift -= 1;
+            }
+            int rShift = median + 1;
+            while (arr[rShift] == value && rShift <= rBorder) {
+                count += 1;
+                rShift += 1;
+            }
+            return count;
+        }
+    }
+    return 0;
+}
 }
